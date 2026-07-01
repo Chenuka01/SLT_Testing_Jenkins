9cf2221d868e4334ddfe08963be49c6319be15cf
Chenuka01 <it23653986@my.sliit.lk>
2026-07-01 21:45:09 +0530

    Implement and apply Route Guard to protect routes from unauthenticated access

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,13 +6,14 @@ import { CredentialList } from './components/credential-list/credential-list';
 import { ActivityLog } from './components/activity-log/activity-log';
 import { BreakGlassComponent } from './components/project-details/break-glass';
 import { MfaSetupComponent } from './components/mfa-setup/mfa-setup';
+import { authGuard } from './services/auth.guard';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'projects', component: ProjectListComponent },
-  { path: 'credentials', component: CredentialList },
-  { path: 'logs', component: ActivityLog },
-  { path: 'break-glass', component: BreakGlassComponent },
-  { path: 'mfa-setup', component: MfaSetupComponent },
+  { path: 'projects', component: ProjectListComponent, canActivate: [authGuard] },
+  { path: 'credentials', component: CredentialList, canActivate: [authGuard] },
+  { path: 'logs', component: ActivityLog, canActivate: [authGuard] },
+  { path: 'break-glass', component: BreakGlassComponent, canActivate: [authGuard] },
+  { path: 'mfa-setup', component: MfaSetupComponent, canActivate: [authGuard] },
   { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
